9b789587ac34b9a8a16f5f87b54aa7778cc8fb35
Asa Salcido <asasalcido14@email.arizona.edu>
2020-08-31 15:37:48 -0700

    Created a questions js and modified logic and index

--- logic.js
+++ logic.js
@@ -1,6 +1,20 @@
 const startBtn = document.getElementById('start')
  
+const questions = [
+    {
+        questionText: "What is the capitol of France",
+        answers: ["Paris", "Versailles"],
+        correctAnswer: "Paris"
+    },
+    {
 
+    }
+]
+
+var questionEl = document.getElementById('question')
+
+for(var i = 0; i < questions.length; i++) {}
+questionEl.innerHTML = question.questionText;
 
 // In my Java I will need to 
     // create my elements: timer, high scores, and start button
---
+++ questions.js
@@ -0,0 +1,29 @@
+const questions = [
+    {
+      title: "HTML files can be created in VS code by inserting which combination of keys?",
+      choices: ["make a page", "css", "html:5", "numbers"],
+      answer: "html:5"
+    },
+    {
+      title: "When you link a CSS page to the HTML you must put it in which section?",
+      choices: ["head", "body", "footer", "section"],
+      answer: "head"
+    },
+    {
+      title: "We need to link our JavaScript file to our html, how would we syntax that?",
+      choices: [
+        "write java in the head section",
+        "drag the file over",
+        "insert bootstrap link",
+        "use script tag at the bottom of the body section"
+      ],
+      answer: "use script tag at the bottom of the body section"
+    },
+    {
+      title:
+        "To clone a repo into a VScode file we type in what to our command line before we paste the address?",
+      choices: ["git add", "git clone", "git copy", "git status"],
+      answer: "quotes"
+    },
+
+  ];
