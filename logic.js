4bf9011ff2d91ca18cbd2868d5251e2a136cdee5
Asa Salcido <asasalcido14@email.arizona.edu>
2020-08-31 14:37:27 -0700

    first push W pseudocode

---
+++ logic.js
@@ -0,0 +1,11 @@
+const startBtn = document.getElementById('start')
+ 
+
+
+// In my Java I will need to 
+    // create my elements: timer, high scores, and start button
+    // an array of questions
+    // buttons for answers on my questions
+    // a library of results that know which answers are right and which are wrong
+    // result tracker (score keeper)
+    //
